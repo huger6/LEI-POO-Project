27b3578a63d1b763fd34d001d5f6bf45777e8876
sanitolina <gugaphone06@gmail.com>
2025-11-05 13:54:45 +0000

    Configs outra vez

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,14 @@
 #include "fileSystem.hpp"
+#include "systemConfig.hpp"
 
 int main() {
     string path = "C:\\Users\\hugoa\\Desktop\\LEI-U\\PINT";
     FileSystem sys(path);
+
+    SystemConfig::setUTF8Locale();
+
+    FileSystem fs;
+    fs.load("C:\\Users\\Projects");
+
+    return 0;
 }
