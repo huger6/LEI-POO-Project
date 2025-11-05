89df5949ada3fd7fae037f2b944bd5266ed09389
sanitolina <gugaphone06@gmail.com>
2025-11-05 13:37:18 +0000

    Configs

---
+++ app/include/systemConfig.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <locale>
+#include <iostream>
+
+class systemConfig{
+    public:
+        static void setUTF8Locale() {
+            try{
+                std::locale utf8Locale("en_US.UTF-8");
+                std::locale::global (utf8Locale);
+                std::wcout.imbue(utf8Locale);
+                std::wcin.imbue(utf8Locale);
+                std::wclog.imbue(utf8Locale);
+
+
+                #ifdef _WIN32
+                    system("chcp 65001 > nul");
+
+                #endif
+                    std::wcout << L"Locale sucessfully set to UTF-8.\n";
+            }
+            catch(const std::runtime_error& e) {
+                std::cerr << "Failed to set UTF-8 locale:" << e.what() << '\n';
+            }
+        }
+    
+};
