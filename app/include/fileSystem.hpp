92a221cf958e914eb50f34bd6b1fe2f1153fe15d
matpt10 <mateusfriassilva10@gmail.com>
2025-11-24 10:33:13 +0000

    Ordered exercises

--- app/include/fileSystem.hpp
+++ app/include/fileSystem.hpp
@@ -27,7 +27,7 @@ class FileSystem {
         std::uint32_t countFolders() const; // 3
         std::uintmax_t memory() const; // 4
         
-        std::string *mostElementsFolder() const; // 4
+        std::string *mostElementsFolder() const; // 5
         std::string *leastElementsFolder() const; // 6
         std::string *largestFile() const; // 7
         std::string *largestFolder() const; // 8
@@ -37,15 +37,15 @@ class FileSystem {
         bool readFromXML(const std::string &s); // 12
         
         // File operations
+        bool removeAll(const std::string &name, ElementType type); // 10
         bool moveFile(const std::string &file, const std::string &newDir); // 13
         bool moveFolder(const std::string &oldDir, const std::string &newDir); // 14
-        bool copyBatch(const std::string &pattern, const std::string &originDir, const std::string &destinDir); // 21
-        bool removeAll(const std::string &name, ElementType type); // 10
-        void renameAllFiles(const std::string &currentName, const std::string &newName); // 19
         std::string *getFileDate(const std::string &file); // 15
+        void renameAllFiles(const std::string &currentName, const std::string &newName); // 19
+        bool copyBatch(const std::string &pattern, const std::string &originDir, const std::string &destinDir); // 21
         
         // Search operations
-        std::optional<std::string> search(const std::string &name, ElementType type);
+        std::optional<std::string> search(const std::string &name, ElementType type); // 9
         void searchAllFolders(std::list<std::string> &li, const std::string &folder) const; // 17
         void searchAllFiles(std::list<std::string> &li, const std::string &file) const; // 18
 
